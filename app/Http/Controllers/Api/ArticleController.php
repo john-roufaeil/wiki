20ff841f3166a8a6a5841f6914464f82a00be92b
john-roufaeil <john.f.roufaeil@gmail.com>
2026-05-16 19:01:12 +0300

    feat(Article): implement Article API

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Resources\ArticlesResource;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $per_page = request()->query('per_page', 10);
+        $articles = Article::with('author')->paginate($per_page);
+        return response()->json(
+            ArticlesResource::collection($articles)->toResponse(request())->getData(),
+            200,
+            [],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    public function show(string $slug)
+    {
+        $article = Article::with('author')->where('slug', $slug)->first();
+
+        if (!$article) {
+            return response()->json([
+                "status" => "failed",
+                "message" => "Article not found"
+            ]);
+        }
+
+        return response()->json(
+            ArticlesResource::make($article)->toResponse(request())->getData(),
+            200,
+            [],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    public function store(StoreArticleRequest $request)
+    {
+        $article = Article::create($request->validated());
+        return response()->json(
+            ArticlesResource::make($article)->toResponse(request())->getData(),
+            200,
+            [],
+            JSON_PRETTY_PRINT
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/articles', [ArticleController::class, 'index']);
+    Route::get('/articles/{id}', [ArticleController::class, 'show']);
+    Route::post('/articles', [ArticleController::class, 'store']);
 });
